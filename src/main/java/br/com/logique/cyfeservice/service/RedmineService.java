3a85bdfb810f1862bdaa94fa14b76b7e4da50059
Yuri Aguiar <yuyu.te319@gmail.com>
2016-07-14 12:46:01 -0300

    Corrected a bug in the openedIssuesForMoreThanXHoursByProject method.

--- src/main/java/br/com/logique/cyfeservice/service/RedmineService.java
+++ src/main/java/br/com/logique/cyfeservice/service/RedmineService.java
@@ -63,6 +63,27 @@ public class RedmineService {
         return issuesInExecutionByProject;
     }
 
+    public Map<String, String> issuesInExecutionByProjectIdV2(Integer projectId) throws RedmineException {
+        RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri, apiAccessKey);
+        IssueManager issueManager = mgr.getIssueManager();
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("project_id", String.valueOf(projectId));
+        parameters.put("status_id", StatusIssue.IN_EXECUTION.getIdStr());
+        Iterator<Issue> iterator = new IteratorIssues.Builder().withParameters(parameters).build(issueManager);
+        Map<String, String> issuesInExecutionByProjectMap = new TreeMap<>();
+        while (iterator.hasNext()) {
+            Issue issue = iterator.next();
+            if (issue.getParentId() == null) {
+                String issuesId = issue.getId().toString();
+                if (issuesInExecutionByProjectMap.get(issuesId) == null) {
+                    issuesInExecutionByProjectMap.put(issuesId, "0");
+                }
+                issuesInExecutionByProjectMap.put(issuesId, issue.getSubject());
+            }
+        }
+        return issuesInExecutionByProjectMap;
+    }
+
     /**
      * Sums the number of closed issues in a day of a single project from all days of a set time interval and puts in
      * an ordered map of total closed issues by day
@@ -266,8 +287,8 @@ public class RedmineService {
         int sum = 0;
         while (iterator.hasNext()) {
             Issue issue = iterator.next();
-            if (issue.getCreatedOn() != null && issue.getClosedOn() != null) {
-                if (DateUtil.diffHour(issue.getCreatedOn(), issue.getClosedOn()) > xHours && issue.getParentId() == null)
+            if (issue.getCreatedOn() != null) {
+                if (DateUtil.diffHour(issue.getCreatedOn(), Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant())) > xHours && issue.getParentId() == null)
                     sum++;
             }
         }
